4990acf5e2cd96fdffbb7ea7940a1a46d07498ee
Juan Manuel Cuellar <juan.cuellar@becall.local>
2025-03-26 09:38:00 +0100

    Set up Linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            indent: ['warn', 4],
+            semi: ['warn', 'always'],
+            quotes: ['warn', 'single'],
+        }
+    }
+]);
